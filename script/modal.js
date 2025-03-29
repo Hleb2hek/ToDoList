94717b9cf865247bb29b6a5d1a0057506706edd9
hleb2hek <hleb2hek@gmail.com>
2025-03-29 17:45:56 +0300

    main

---
+++ script/modal.js
@@ -0,0 +1,44 @@
+const modalController = function ({btnOpen,btnClose,modalWindow}) {
+	const btnElemOpen = document.querySelector(btnOpen);
+	const btnElemClose = document.querySelector(btnClose);
+	// const btnElemApply = document.querySelector('[data-btn-apply]');
+	
+	const modalElem = document.querySelector(modalWindow);
+	
+	modalElem.style.cssText = `
+		display: flex;
+		visibility: hidden;
+		opacity: 0;
+		transition: opacity .1s ease-in-out;
+	`;
+	
+	const closeModal = event => {
+		const target = event.target;
+		if (
+			target === btnElemClose ||
+			target.closest(modalWindow) ||
+			event.code === 'Escape'
+		){
+			modalElem.style.opacity = 0;
+			setTimeout(() => {
+				modalElem.style.visibility = 'hidden';
+			}, 200);
+			window.removeEventListener('keydown',closeModal);
+		}
+	}
+	
+	const showModal = function() {
+		modalElem.style.visibility = 'visible';
+		modalElem.style.opacity = 1;
+		window.addEventListener('keydown',closeModal)
+	}
+	
+	btnElemOpen.addEventListener('click', showModal);
+	modalElem.addEventListener('click', closeModal)
+}
+
+modalController({
+	btnOpen: '[data-btn-create-note]',
+	btnClose: '[data-btn-close]',
+	modalWindow: '[data-modal]'
+});

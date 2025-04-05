e6bd008709e5de0e620211db31fce9f12ba06190
Hleb2hek <hleb2hek@gmail.com>
2025-04-05 10:14:21 +0300

    main

--- script/item.js
+++ script/item.js
@@ -1,78 +1,25 @@
+// Импортируем showModal
+import { showModal } from "./modal.js";
 
+// Создаём переменную, которая является списком динамических элементов
+const ulElement = document.querySelector('[data-hero-ul-list]');
 
-// Изолируем код
-const setingsItem = function() {
-	// переменная ul.hero__list
-	const ulElement = document.querySelector('[data-hero-ul-list]');
+// Вешаем событие, которое при клике отсылается к нажатию на модалку или удаление
+ulElement.addEventListener('click', (event) => {
+	const target = event.target;
+	// При нажатии проверяем, что нажато и в случае
+	// если есть вложенные элемент в структуре найдётся за счёт closest
+	const penBtn = target.closest('[data-pen-btn]');
+	const trashBtn = target.closest('[data-trash-btn]');
 
-	// переменная button.hero__btn.btn
-	const btnElemApply = document.querySelector('[data-btn-apply]');
-
-	// input.modal__input.input
-	const inputElement = document.querySelector('[data-modal-input]');
-
-	const createItem = function() {
-		let liElement = document.createElement('li');
-		liElement.className ='hero__item';
-
-		let noteTitle = inputElement.value.trim();
-		let warningText = inputElement.parentNode.querySelector('.modal__text');
-
-		if (noteTitle === '') {
-			if(!warningText) {
-				let textElement = document.createElement('p');
-				textElement.classList.add('modal__text');
-				inputElement.classList.add('input__warning');
-
-				btnElemApply.disabled = true;
-
-				textElement.innerHTML = 'The input field is empty';
-				setTimeout (() => {
-					textElement.innerHTML = 'Enter the task name';
-				},3000);
-	
-				inputElement.insertAdjacentElement('afterend', textElement);
-
-			}
-		return
-		}
-
-		inputElement.classList.remove('input__warning')
-		btnElemApply.disabled = false;
-
-		if (warningText) {
-			warningText.remove();
-		}
-		liElement.innerHTML = `
-				<form class="hero__description">
-					<input class="hero__input input input__checkbox" type="checkbox" id="" >
-					<h2 class="hero__title">${noteTitle}</h2>
-				</form>
-				<div class="hero__option">
-				<button type="button" class="hero__pen btn btn__option btn__option--pen">
-					<svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-					<path d="M8.67272 5.99106L2 12.6637V16H5.33636L12.0091 9.32736M8.67272 5.99106L11.0654 3.59837L11.0669 3.59695C11.3962 3.26759 11.5612 3.10261 11.7514 3.04082C11.9189 2.98639 12.0993 2.98639 12.2669 3.04082C12.4569 3.10257 12.6217 3.26735 12.9506 3.59625L14.4018 5.04738C14.7321 5.37769 14.8973 5.54292 14.9592 5.73337C15.0136 5.90088 15.0136 6.08133 14.9592 6.24885C14.8974 6.43916 14.7324 6.60414 14.4025 6.93398L14.4018 6.93468L12.0091 9.32736M8.67272 5.99106L12.0091 9.32736" stroke-linecap="round" stroke-linejoin="round"/>
-					</svg>
-				</button>
-				<button type="button" class="hero__trash btn btn__option btn__option--trash">
-					<svg width="18" height="18" viewBox="0 0 18 18" xmlns="http://www.w3.org/2000/svg">
-					<path d="M3.87414 7.61505C3.80712 6.74386 4.49595 6 5.36971 6H12.63C13.5039 6 14.1927 6.74385 14.1257 7.61505L13.6064 14.365C13.5463 15.1465 12.8946 15.75 12.1108 15.75H5.88894C5.10514 15.75 4.45348 15.1465 4.39336 14.365L3.87414 7.61505Z" />
-					<path d="M14.625 3.75H3.375" stroke-linecap="round"/>
-					<path d="M7.5 2.25C7.5 1.83579 7.83577 1.5 8.25 1.5H9.75C10.1642 1.5 10.5 1.83579 10.5 2.25V3.75H7.5V2.25Z"/>
-					<path d="M10.5 9V12.75" stroke-linecap="round"/>
-					<path d="M7.5 9V12.75" stroke-linecap="round"/>
-					</svg>
-				</button>
-		`;
-		ulElement.appendChild(liElement);
-		inputElement.value = '';
+	// Если нажата penBtn, то показать модалку
+	if (penBtn) {
+		showModal();
 	}
-	inputElement.addEventListener('input', () => {
-		btnElemApply.disabled = inputElement.value.trim() === '';
-	})
-	btnElemApply.addEventListener('click',createItem);
-
-}
-
-setingsItem();
-
+	// Если нажата trashBtn, то создать переменную, которая найдёт родительский 
+	// элемент .hero__item и удалит
+	if (trashBtn) {
+		const liElement = trashBtn.closest('.hero__item');
+		liElement.remove();
+	}
+});
--- script/modal.js
+++ script/modal.js
@@ -1,10 +1,15 @@
 
-const modalController = function ({btnOpen,btnClose,modalWindow}) {
+export const modalController = function ({btnOpen,btnClose,modalWindow}) {
+
 	const btnElemOpen = document.querySelector(btnOpen);
 	const btnElemClose = document.querySelector(btnClose);
-	
 	const modalElem = document.querySelector(modalWindow);
 	
+	const ulElement = document.querySelector('[data-hero-ul-list]');
+	const btnApply = document.querySelector('[data-btn-apply]');
+	const inputElement = document.querySelector('[data-modal-input]');
+
+	// Прописываем модалки изначальные стили
 	modalElem.style.cssText = `
 		display: flex;
 		visibility: hidden;
@@ -12,33 +17,139 @@ const modalController = function ({btnOpen,btnClose,modalWindow}) {
 		transition: opacity .1s ease-in-out;
 	`;
 	
+	const warningWrapper = function(firstText, secondText = null, sec = 1500) {
+		if(!document.querySelector('.modal__text')) {
+			// Добавить элемент
+			let textElement = document.createElement('p');
+			// Добавить класс
+			textElement.classList.add('modal__text');
+			inputElement.classList.add('input__warning');
+
+			// Заблокировать кнопку
+			btnApply.disabled = true;
+
+			// Добавляем содержимое textElement
+			textElement.innerHTML = firstText;
+
+			// Через sec меняем содержимое
+			if(secondText) {
+				setTimeout (() => {
+					textElement.innerHTML = secondText;
+				},sec);
+			}
+			// Встраиваем после inputElement переменную textElement
+			inputElement.insertAdjacentElement('afterend', textElement);
+		}
+	}
+
+	// Функция закрытия 
 	const closeModal = event => {
 		const target = event.target
-		if (
-			target === btnElemClose ||
-			target === modalElem ||
-			event.code === 'Escape'
-		){
+
+		// Если нажата одна из кнопок или клавиш
+		if ( target === btnElemClose || target === modalElem ||event.code === 'Escape') {
+
+			// Делаем модалку не видимной 
 			modalElem.style.opacity = 0;
+			// Через 200ms модалка красиво исчезнет 
 			setTimeout(() => {
 				modalElem.style.visibility = 'hidden';
 			}, 200);
+			// Удаляем обработчик событий для кнопки, чтобы не было висячего обработчика
 			window.removeEventListener('keydown',closeModal);
 		}
 	}
 	
+	// Функция открытия модалки
 	const showModal = function() {
+		// Добавляем свойства, чтобы было видно модалку
 		modalElem.style.visibility = 'visible';
 		modalElem.style.opacity = 1;
+		// Добавляем на модалку обработчик событий
 		window.addEventListener('keydown',closeModal)
 	}
+
+	// Функция на создание динамического элемента
+	const createItem = function() {
+		// Создаём главный дин.элемент
+		let liElement = document.createElement('li');
+		// Даём ему класс
+		liElement.className ='hero__item';
+
+		// Полученое значение мы обрабатываем от пробелов
+		let noteTitle = inputElement.value.trim();
+		// Находим родительский элемент inputElement через .modal__text
+		let warningText = inputElement.parentNode.querySelector('.modal__text');
+
+
+		
+		// Если пустая строка
+		if (noteTitle === '') {
+			warningWrapper('The input field is empty','Enter the task name',1500)
+			// Возвращаем результат
+			return
+		// Если строка превышает 30 символов
+		} else if (noteTitle.length > 30) {
+			warningWrapper ('The text is more than 30 characters long')
+			// Возвращаем результат
+			return
+		} else {
+			// Если нет пустой строки, то убираем класс
+			inputElement.classList.remove('input__warning')
+			// Разблокировем кнопку
+			btnApply.disabled = false;
+	
+			// Если предупреждающий текст есть, то удалить
+			if (warningText) {
+				warningText.remove();
+			}
+			// Добавляемый динамический элемент
+			liElement.innerHTML = `
+					<form class="hero__description">
+						<input class="hero__input input input__checkbox" type="checkbox" id="" >
+						<h2 class="hero__title">${noteTitle}</h2>
+					</form>
+					<div class="hero__option">
+					<button type="button" class="hero__pen btn btn__option btn__option--pen" data-pen-btn>
+						<svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+						<path d="M8.67272 5.99106L2 12.6637V16H5.33636L12.0091 9.32736M8.67272 5.99106L11.0654 3.59837L11.0669 3.59695C11.3962 3.26759 11.5612 3.10261 11.7514 3.04082C11.9189 2.98639 12.0993 2.98639 12.2669 3.04082C12.4569 3.10257 12.6217 3.26735 12.9506 3.59625L14.4018 5.04738C14.7321 5.37769 14.8973 5.54292 14.9592 5.73337C15.0136 5.90088 15.0136 6.08133 14.9592 6.24885C14.8974 6.43916 14.7324 6.60414 14.4025 6.93398L14.4018 6.93468L12.0091 9.32736M8.67272 5.99106L12.0091 9.32736" stroke-linecap="round" stroke-linejoin="round"/>
+						</svg>
+					</button>
+					<button type="button" class="hero__trash btn btn__option btn__option--trash" data-trash-btn>
+						<svg width="18" height="18" viewBox="0 0 18 18" xmlns="http://www.w3.org/2000/svg">
+						<path d="M3.87414 7.61505C3.80712 6.74386 4.49595 6 5.36971 6H12.63C13.5039 6 14.1927 6.74385 14.1257 7.61505L13.6064 14.365C13.5463 15.1465 12.8946 15.75 12.1108 15.75H5.88894C5.10514 15.75 4.45348 15.1465 4.39336 14.365L3.87414 7.61505Z" />
+						<path d="M14.625 3.75H3.375" stroke-linecap="round"/>
+						<path d="M7.5 2.25C7.5 1.83579 7.83577 1.5 8.25 1.5H9.75C10.1642 1.5 10.5 1.83579 10.5 2.25V3.75H7.5V2.25Z"/>
+						<path d="M10.5 9V12.75" stroke-linecap="round"/>
+						<path d="M7.5 9V12.75" stroke-linecap="round"/>
+						</svg>
+					</button>
+			`;
+			// Добавляем конец дочерний элемент liElement
+			ulElement.appendChild(liElement);
+			// Обнуляем поле input
+			inputElement.value = '';
+		}
+		
+
+	}
+
+	inputElement.addEventListener('input', () => {
+		btnApply.disabled = inputElement.value.trim() === '';
+	})
+	btnApply.addEventListener('click',createItem);
 	
 	btnElemOpen.addEventListener('click', showModal);
-	modalElem.addEventListener('click', closeModal)
+	modalElem.addEventListener('click', closeModal);
+	
+	return { showModal }
+
 }
 
-modalController({
+const modal = modalController({
 	btnOpen: '[data-btn-create-note]',
 	btnClose: '[data-btn-close]',
 	modalWindow: '[data-modal]'
 });
+
+export const { showModal } = modal;
